ec3e4dca8ca3931762ab66ad9d1dcff239759897
Phil Massyn <massyn@users.noreply.github.com>
2018-08-05 21:23:43 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,12 @@
 
 INSTALL_DIR=/usr/local/
 
+# == we must run as root
+if [[ `whoami` != 'root' ]]; then
+        echo "You need to run this as root";
+        exit 1
+fi
+
 if [[ ! -d $INSTALL_DIR ]]; then
   echo "Installation directory $INSTALL_DIR does not exist"
   exit 1
@@ -19,4 +25,9 @@ chmod +x $INSTALL_DIR/config.sh
 cp cronwrapper.sh $INSTALL_DIR
 chmod +x $INSTALL_DIR/cronwrapper.sh
 
+cp maintain_os.sh $INSTALL_DIR
+chmod +x $INSTALL_DIR/maintain_os.sh
+
 bash $INSTALL_DIR/config.sh
+
+bash install_crontab.sh
