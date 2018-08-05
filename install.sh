91ad4c6c21f9a247fdde34f75c35514c4214a990
Phil Massyn <massyn@users.noreply.github.com>
2018-08-05 21:31:09 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 
-INSTALL_DIR=/usr/local/
-
 # == we must run as root
 if [[ `whoami` != 'root' ]]; then
         echo "You need to run this as root";
         exit 1
 fi
+
+# == setup the config file first, so we know where things go
+bash config.sh
+
+if [[ ! -f /etc/server-build.sh ]]; then
+        echo "You need to run config.sh first"
+        exit 1
+fi
+. /etc/server-build.sh
+
+INSTALL_DIR=$app_path
 
 if [[ ! -d $INSTALL_DIR ]]; then
   echo "Installation directory $INSTALL_DIR does not exist"
@@ -27,7 +36,5 @@ chmod +x $INSTALL_DIR/cronwrapper.sh
 
 cp maintain_os.sh $INSTALL_DIR
 chmod +x $INSTALL_DIR/maintain_os.sh
-
-bash $INSTALL_DIR/config.sh
 
 bash install_crontab.sh
