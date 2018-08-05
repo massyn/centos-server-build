4f10abafed574593c1db322de397a6aeb73d5647
Phil Massyn <massyn@users.noreply.github.com>
2018-08-05 21:33:53 +0900

    Update install_crontab.sh

--- install_crontab.sh
+++ install_crontab.sh
@@ -8,5 +8,16 @@ function addtocrontab () {
         cat <(fgrep -i -v "$command" <(crontab -l)) <(echo "$job") | crontab -
 }
 
-addtocrontab "0 0 * * 0" "/usr/local/cronwrapper.sh maintain_os /usr/local/maintain_os.sh"
+if [[ `whoami` != 'root' ]]; then
+        echo "You need to run this as root";
+        exit 1
+fi
+
+if [[ ! -f /etc/server-build.sh ]]; then
+        echo "You need to run config.sh first"
+        exit 1
+fi
+. /etc/server-build.sh
+
+addtocrontab "0 0 * * 0" "$app_path/cronwrapper.sh maintain_os $app_path/maintain_os.sh"
 
