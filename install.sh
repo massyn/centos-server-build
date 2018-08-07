e59e95ca14e195a3c331671869d4f165249a65a3
Phil Massyn <massyn@users.noreply.github.com>
2018-08-07 21:55:26 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,17 +15,27 @@ if [[ ! -f /etc/server-build.sh ]]; then
 fi
 . /etc/server-build.sh
 
-INSTALL_DIR=$app_path
+if [[ ! -d $app_path ]]; then
+        mkdir $app_path
+fi
 
-if [[ ! -d $INSTALL_DIR ]]; then
-        mkdir $INSTALL_DIR
+if [[ ! -d $log_path ]]; then
+        mkdir $log_path
+        chown 0:0 $log_path
+        chmod 600 $log_path
+fi
+
+if [[ ! -d $backup_path ]]; then
+        mkdir $backup_path
+        chown 0:0 $backup_path
+        chmod 600 $backup_path
 fi
 
 for name in $(echo setup_web.sh letsencrypt.sh config.sh cronwrapper.sh maintain_os.sh maintain_av.sh setup_db.sh)
 do
         echo $name
-        cp $name $INSTALL_DIR
-        chmod +x $INSTALL_DIR/$name
+        cp $name $app_path
+        chmod +x $app_path/$name
 done
 
 bash install_crontab.sh
