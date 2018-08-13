5359ea541c7bbc4001b5c0799a5cbd67b5dada9f
Phil Massyn <massyn@users.noreply.github.com>
2018-08-13 13:11:48 +0900

    Update setup_web.sh

--- setup_web.sh
+++ setup_web.sh
@@ -116,7 +116,7 @@ if [[ ! -z $site ]]; then
         echo "  add_header X-Frame-Options SAMEORIGIN;" >> $cfg
         echo "  add_header X-Content-Type-Options nosniff;" >> $cfg
         echo "  add_header X-XSS-Protection \"1; mode=block\";" >> $cfg
-        echo "  add_header Content-Security-Policy \"script-src 'strict-dynamic' 'nonce-rAnd0m123' 'unsafe-inline' http: https:;object-src 'none';base-uri 'none'\";" >> $cfg
+        echo "  add_header Content-Security-Policy \"script-src 'nonce-rAnd0m123' 'unsafe-inline' http: https:;object-src 'none';base-uri 'none'\";" >> $cfg
         echo "  add_header Strict-Transport-Security \"max-age=31536000; includeSubdomains; preload\";" >> $cfg
         echo "  add_header Referrer-Policy same-origin;" >> $cfg
 
